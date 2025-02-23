4094ddbcf7da740a5ff68e57630cd00209865e65
Vincent Tjeng <vincent.tjeng@gmail.com>
2025-02-23 00:35:32 -0800

    Inline PHOTO_GALLERY_CONFIG and PHOTO_GALLERY_DATA. (#33)
    This is a temporary patch because `.js` files referenced in a `.js` file don't seem to be included by Jekkyl.

--- assets/js/photo-gallery-config.js
+++
@@ -1,4 +0,0 @@
-export const PHOTO_GALLERY_CONFIG = {
-  PREVIEW_BASE_URL: "https://storage.googleapis.com/vtjeng-photos/previews/",
-  FULL_RES_BASE_URL: "https://storage.googleapis.com/vtjeng-photos/full_res/",
-};
